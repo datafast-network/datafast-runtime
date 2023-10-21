6e589233e8a25a5d9e6e53c444e0f83b90df62d9
vutran1710 <me@vutr.io>
2023-10-21 08:49:29 +0700

    update

--- src/chain/ethereum/transaction.rs
+++ src/chain/ethereum/transaction.rs
@@ -1,11 +1,13 @@
 use super::asc::*;
 
+use crate::asc::base::AscIndexId;
 use crate::asc::base::AscPtr;
+use crate::asc::base::IndexForAscTypeId;
 use crate::asc::native_types::Uint8Array;
 use crate::impl_asc_type_struct;
 
 #[repr(C)]
-pub(crate) struct Transaction {
+pub struct AscTransaction {
     pub hash: AscPtr<AscH256>,
     pub index: AscPtr<AscBigInt>,
     pub from: AscPtr<AscH160>,
@@ -17,8 +19,12 @@ pub(crate) struct Transaction {
     pub nonce: AscPtr<AscBigInt>,
 }
 
+impl AscIndexId for AscTransaction {
+    const INDEX_ASC_TYPE_ID: IndexForAscTypeId = IndexForAscTypeId::EthereumTransaction;
+}
+
 impl_asc_type_struct!(
-    Transaction;
+    AscTransaction;
     hash => AscPtr<AscH256>,
     index => AscPtr<AscBigInt>,
     from => AscPtr<AscH160>,
@@ -29,3 +35,28 @@ impl_asc_type_struct!(
     input => AscPtr<Uint8Array>,
     nonce => AscPtr<AscBigInt>
 );
+
+/*
+/// Convert to Asc Transaction from Query Store
+impl ToAscObj<AscTransaction> for EthereumTransactionData {
+    fn to_asc_obj<H: AscHeap + ?Sized>(
+        &self,
+        heap: &mut H,
+        gas: &GasCounter,
+    ) -> Result<AscTransaction, HostExportError> {
+        Ok(AscTransaction {
+            hash: asc_new(heap, &self.hash, gas)?,
+            index: asc_new(heap, &BigInt::from_unsigned_u128(self.index), gas)?,
+            from: asc_new(heap, &self.from, gas)?,
+            to: self
+                .to
+                .map(|to| asc_new(heap, &to, gas))
+                .unwrap_or(Ok(AscPtr::null()))?,
+            value: asc_new(heap, &BigInt::from_unsigned_u256(&self.value), gas)?,
+            gas_limit: asc_new(heap, &BigInt::from_unsigned_u256(&self.gas_limit), gas)?,
+            gas_price: asc_new(heap, &BigInt::from_unsigned_u256(&self.gas_price), gas)?,
+            input: asc_new(heap, &*self.input, gas)?,
+            nonce: asc_new(heap, &BigInt::from_unsigned_u256(&self.nonce), gas)?,
+        })
+    }
+}*/
