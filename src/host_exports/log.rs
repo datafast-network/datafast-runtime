d18f3d335953d149c91b47a40ff26fbef55a73bb
vutran1710 <me@vutr.io>
2023-10-23 12:05:01 +0700

    up

--- src/host_exports/log.rs
+++ src/host_exports/log.rs
@@ -1,29 +1,56 @@
 use log;
-use wasmer::{FunctionEnvMut, FunctionType, Store, Type, WasmError};
+use wasmer::{
+    AsStoreMut, AsStoreRef, FunctionEnvMut, FunctionType, Memory, MemoryError, MemoryType,
+    RuntimeError, Store, Type, Value, WasmError,
+};
 
+use super::Env;
 use crate::asc::{
-    base::{AscType, FromAscObj},
+    base::{asc_get, AscType, FromAscObj},
     native_types::string::AscString,
 };
 
-pub fn log_log(log_level: i32, message: i32) -> Result<(), WasmError> {
+pub const LOG_TYPE: ([Type; 2], [Type; 0]) = ([Type::I32, Type::I32], []);
+
+pub fn log_log(mut env: FunctionEnvMut<Env>, args: &[Value]) -> Result<Vec<Value>, RuntimeError> {
     // How to access store / memory
     // memory.view()[ptr] -> real data
-    let data = message.to_be_bytes();
-    let asc_string = AscString::from_asc_bytes(&data).unwrap();
-    let string_msg = String::from_utf16(asc_string.content()).unwrap();
-    log::info!("{string_msg}");
-    Ok(())
+    let log_level = args[0].clone();
+    let ptr = args[1].clone().i32().unwrap();
+
+    log::info!("{:?}", args);
+
+    let mut store_mut = env.as_store_mut();
+    let memory = Memory::new(&mut store_mut, MemoryType::new(1, Some(1), false)).unwrap();
+
+    let store_ref = env.as_store_ref();
+    let view = memory.view(&store_ref);
+
+    log::info!("Memory size (pages) {:?}", view.size());
+    log::info!("Memory size (bytes) {:?}", view.size().bytes());
+    log::info!("Memory size (data-size) {:?}", view.data_size());
+    // let m = env.memory;
+
+    let mut new_vec = Vec::<u8>::new();
+    let mut buf = new_vec.as_mut_slice();
+    let str_vec = view.read(0, &mut buf).unwrap();
+
+    // Convert the subslice to a `&str`.
+    log::info!("buffer: {:?}", buf);
+    let msg = AscString::from_asc_bytes(buf).unwrap();
+    let msg = String::from_utf16(msg.content()).unwrap();
+    log::info!("message: {msg}");
+    Ok(vec![])
 }
 
 pub fn sum(_env: FunctionEnvMut<()>, a: i32, b: i32) -> i32 {
-    log::info!("{a}, {b}");
     a + b
 }
 
 #[cfg(test)]
 mod test {
     use super::super::create_host_instance;
+    use super::*;
     use env_logger;
 
     #[test]
@@ -35,6 +62,6 @@ mod test {
         .unwrap();
         let f = instance.exports.get_function("myown").unwrap();
         log::info!("-- calling");
-        f.call(&mut store, &[]).unwrap();
+        let ptr = f.call(&mut store, &[]).unwrap();
     }
 }
--- src/host_exports/mod.rs
+++ src/host_exports/mod.rs
@@ -3,27 +3,39 @@ pub mod log;
 use wasmer::imports;
 use wasmer::Function;
 use wasmer::FunctionEnv;
+use wasmer::FunctionEnvMut;
+use wasmer::FunctionType;
 use wasmer::Instance;
+use wasmer::Memory;
+use wasmer::MemoryType;
 use wasmer::Module;
 use wasmer::Store;
+use wasmer::Type;
 
 use crate::conversion;
 use crate::global;
 use crate::store;
 
-struct Env {}
+pub struct Env {
+    pub memory: Memory,
+}
+
+fn sum(_env: FunctionEnvMut<()>, a: i32, b: i32) -> i32 {
+    a + b
+}
 
 pub fn create_host_instance(
     wasm_path: &str,
 ) -> Result<(Store, Instance), Box<dyn std::error::Error>> {
     let wasm_bytes = std::fs::read(wasm_path)?;
     let mut store = Store::default();
+    let memory = Memory::new(&mut store, MemoryType::new(1, None, false)).unwrap();
     let module = Module::new(&store, wasm_bytes)?;
 
     // Global
     let abort = Function::new(&mut store, global::ABORT_TYPE, global::abort);
 
-    let env = FunctionEnv::new(&mut store, Env {});
+    let env = FunctionEnv::new(&mut store, Env { memory });
     // Conversion functions
     let big_int_to_hex = Function::new(
         &mut store,
@@ -68,10 +80,13 @@ pub fn create_host_instance(
         store::store_get,
     );
 
+    let memory = Memory::new(&mut store, MemoryType::new(1, None, false)).unwrap();
+
     // Running cargo-run will immediately tell which functions are missing
     let import_object = imports! {
         "env" => {
-            "abort" => abort
+            "abort" => abort,
+            "memory" => memory
         },
         "conversion" => {
             "typeConversion.bigIntToHex" => big_int_to_hex,
@@ -84,7 +99,7 @@ pub fn create_host_instance(
         "index" => {
             "store.set" => store_set,
             "store.get" => store_get,
-            "log.log" => Function::new_typed(&mut store, log::log_log)
+            "log.log" => Function::new_with_env(&mut store, &env, &FunctionType::new(vec![Type::I32, Type::I32], vec![]), log::log_log)
         }
     };
     let instance = Instance::new(&mut store, &module, &import_object)?;
