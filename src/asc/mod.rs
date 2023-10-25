38e584f7c34754ab9f8b82e0d76b2d8dcc063086
vutran1710 <me@vutr.io>
2023-10-25 12:37:42 +0700

    update

--- src/asc/mod.rs
+++ src/asc/mod.rs
@@ -1,7 +1,11 @@
 /// AssemblyScript types
 pub mod base;
-pub mod bignumber;
 pub mod errors;
 pub mod macros;
 pub mod native_types;
+
+/// Extra types
+pub mod bignumber;
+
+/// Type conversion back & forth
 pub mod to_from;
