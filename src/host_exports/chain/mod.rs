f4ecb30370e6544f53d63ec411494edfedc99e79
vutran1710 <me@vutr.io>
2023-10-27 15:15:13 +0700

    update test

--- src/host_exports/chain/mod.rs
+++ src/host_exports/chain/mod.rs
@@ -1,12 +1,12 @@
 #[cfg(test)]
 mod test {
-    use wasmer::Value;
-
-    use super::super::asc::*;
     use crate::asc::base::asc_new;
-    use crate::asc::base::ToAscObj;
     use crate::chain::ethereum::block::EthereumBlockData;
     use crate::host_exports::test::*;
+    use ethabi::ethereum_types::H256;
+    use ethabi::ethereum_types::U64;
+    use std::str::FromStr;
+    use wasmer::Value;
 
     #[::rstest::rstest]
     // #[case("0.0.4")]
@@ -19,6 +19,10 @@ mod test {
 
         let mut host = mock_host_instance(version, &wasm_path);
         let mut block = EthereumBlockData::default();
+        block.number = U64::from_str_radix("153453", 10).unwrap();
+        block.hash =
+            H256::from_str("0xfe52a399d93c48b67bb147432aff55873576997d9d05de2c97087027609ae440")
+                .unwrap();
         let asc_block = asc_new(&mut host, &mut block).unwrap();
 
         let ptr = asc_block.wasm_ptr();
