d05f4a8270b76bdf358638471908eb9b57717258
vutran1710 <me@vutr.io>
2023-10-28 16:03:51 +0700

    update subgraph mod

--- src/subgraph/mod.rs
+++ src/subgraph/mod.rs
@@ -1,25 +1,63 @@
 use std::collections::HashMap;
-use std::marker::PhantomData;
+use thiserror::Error;
+use wasmer::AsStoreMut;
+use wasmer::AsStoreRef;
+use wasmer::Exports;
+use wasmer::FromToNativeWasmType;
 use wasmer::Instance;
+use wasmer::RuntimeError;
+use wasmer::Store;
 use wasmer::TypedFunction;
-use wasmer::Value;
 
-pub enum HandlerDataType {
-    Block,
-    Transaction,
-    Event,
-    Log,
+use crate::asc::errors::AscError;
+
+#[derive(Debug, Error)]
+pub enum SubgraphErr {
+    #[error(transparent)]
+    RuntimeError(#[from] RuntimeError),
+    #[error(transparent)]
+    AscError(#[from] AscError),
 }
 
-pub struct Handler<T> {
-    inner: TypedFunction<[Value; 1], [Value; 0]>,
-    ty: PhantomData<T>,
+pub trait ChainData: FromToNativeWasmType {
+    fn block() -> Vec<u8>;
+    fn transaction() -> Vec<u8>;
+    fn event() -> Vec<u8>;
 }
 
-pub struct SubgraphSource {
+pub struct Handler<T: ChainData> {
+    name: String,
+    inner: TypedFunction<T, ()>,
+}
+
+impl<T: ChainData> Handler<T> {
+    pub fn new<S: AsStoreRef>(store: S, instance_exports: &Exports, func_name: &str) -> Self {
+        Self {
+            name: func_name.to_string(),
+            inner: instance_exports
+                .get_typed_function(&store, func_name)
+                .expect("No function with such name exists"),
+        }
+    }
+}
+
+pub struct SubgraphSource<T: ChainData> {
     pub id: String,
     pub wasm_instance: Instance,
-    pub handlers: HashMap<String, Handler<HandlerDataType>>,
+    pub handlers: HashMap<String, Handler<T>>,
+    pub store: Store,
 }
 
-pub struct Subgraph(HashMap<String, SubgraphSource>);
+pub struct Subgraph<T: ChainData> {
+    pub sources: HashMap<String, SubgraphSource<T>>,
+    pub id: String,
+}
+
+impl<T: ChainData> Subgraph<T> {
+    pub fn invoke(&mut self, source_id: &str, func: &str, args: T) -> Result<(), SubgraphErr> {
+        let source = self.sources.get_mut(source_id).expect("Bad source id");
+        let mut store = source.store.as_store_mut();
+        let handler = source.handlers.get(func).expect("Bad handler name");
+        handler.inner.call(&mut store, args).map(Ok)?
+    }
+}
