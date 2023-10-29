66bcbf10bc6fafc3dacf79d2e3c684f487424eb4
vutran1710 <me@vutr.io>
2023-10-30 00:36:02 +0700

    Add figment, update config

--- src/config.rs
+++ src/config.rs
@@ -1,1 +1,21 @@
-pub struct Config {}
+use figment::providers::Env;
+use figment::providers::Format;
+use figment::providers::Toml;
+use figment::Figment;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Config {
+    pub manifest: String,
+}
+
+impl Config {
+    pub fn load() -> Self {
+        let config: Config = Figment::new()
+            .merge(Toml::file("config.toml"))
+            .merge(Env::prefixed("SWR_"))
+            .extract()
+            .expect("Failed to init config");
+        return config;
+    }
+}
