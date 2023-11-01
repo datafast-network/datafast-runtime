7de32838bdfd36577ec3f136b09c59fc305db020
vutran1710 <me@vutr.io>
2023-11-01 11:46:15 +0700

    update main

--- src/main.rs
+++ src/main.rs
@@ -23,26 +23,19 @@ use wasm_host::create_wasm_host;
 #[tokio::main]
 async fn main() -> Result<(), SwrError> {
     // 1. Load config & cli-arg
+    // TODO: impl CLI
     let config = Config::load()?;
-
-    // 2. Start ManifestLoader & load manifest bundle (subgraph.yaml/abis/wasm etc)
+    // 2. TODO: impl IPFS Loader
     let manifest = ManifestLoader::new(&config.manifest).await?;
-
-    // 3. Binding db connection
-    // TODO: add DB binding connection, so we can impl store_set & store_get
+    // 3. Binding DB Connection
     let database = Database::new(&config).await?;
-
-    // 4. Create a subgraph-instance first
-    // NOTE: normally subgraph does not have a name. It generally is derived from the hash of the whole manifest set.
-    // when using ManifestLoader to load bundle from IPFS, we might actually grab subgraph-hex-id
-    // but since it is not implemented yet, use config's subgraph_name for id as well
-    // This value is not critical, basically it is meant to help with metrics (prometheus) later
+    // 4. Create a subgraph-instance
     let subgraph_id = config
         .subgraph_id
         .clone()
         .unwrap_or(config.subgraph_name.clone());
     let mut subgraph = Subgraph::new_empty(&config.subgraph_name, &subgraph_id);
-
+    // Creating source & WasmHosts
     for datasource in manifest.datasources() {
         let api_version = datasource.mapping.apiVersion.to_owned();
         let wasm_bytes = manifest.load_wasm(&datasource.name).await?.wasm_bytes;
@@ -55,13 +48,10 @@ async fn main() -> Result<(), SwrError> {
     // 5. Binding blockstore connection
     // TODO: impl blockstore (bus subscription)
 
-    // 6. Creating message transport channel
-    // Receving one mmessage at a time
+    // 6. Creating message transport channel, moving one(1) mmessage at a time
     let (subgraph_msg_sender, subgraph_receiver) = kanal::bounded_async(1);
-
     // 7. Start 2 threads:
-    // - One thread for Input-Data(Block/Event/Log/Tx) Subscriber
-    // TODO: impl
+    // TODO: one thread for Input-Data(Block/Event/Log/Tx) Subscriber
     let subscriber_run = async move {
         ::log::info!("Acquire subgraph_sender: {:?}", subgraph_msg_sender);
         // todo!("Impl data subscription");
@@ -75,6 +65,6 @@ async fn main() -> Result<(), SwrError> {
     ::tokio::select! {
         result = subscriber_run => result,
         result = swr_run => result.map_err(SwrError::from),
-        // TODO: We need prometheus as well
+        // 9. TODO: We need prometheus as well
     }
 }
