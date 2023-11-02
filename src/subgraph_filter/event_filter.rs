c89b95236a171f0575b9d505fae157cb6cc13e05
quannguyen <quanna.dev@gmail.com>
2023-11-02 15:08:49 +0700

    update

--- src/subgraph_filter/event_filter.rs
+++ src/subgraph_filter/event_filter.rs
@@ -1,6 +1,13 @@
+use super::errors::FilterError;
+use crate::chain::ethereum::block::EthereumBlockData;
 use crate::chain::ethereum::event::EthereumEventData;
+use crate::chain::ethereum::transaction::EthereumTransactionData;
+use crate::ingestor_data as pb;
+use crate::subgraph_filter::filter::FilterResult;
 use crate::subgraph_filter::filter::SubgraphFilter;
-
+use std::str::FromStr;
+use web3::types::Address;
+use web3::types::H256;
 #[derive(Clone, Debug)]
 pub struct SubgraphLogData {
     pub name: String,
@@ -17,9 +24,83 @@ impl EventFilter {
     pub fn new(contract: ethabi::Contract, address: ethabi::Address) -> Self {
         Self { contract, address }
     }
-}
 
+    fn parse_event(&self, log: &web3::types::Log) -> FilterResult<SubgraphLogData> {
+        let event = self
+            .contract
+            .events()
+            .find(|event| event.signature() == log.topics[0])
+            .ok_or(FilterError::ParseError(format!(
+                "Invalid signature event {}",
+                log.address
+            )))?;
+        let event_data = event
+            .parse_log(ethabi::RawLog {
+                topics: log.topics.clone(),
+                data: log.data.0.clone(),
+            })
+            .map(|event| EthereumEventData {
+                params: event.params,
+                address: log.address,
+                log_index: log.log_index.unwrap_or_default(),
+                transaction_log_index: log.transaction_log_index.unwrap_or_default(),
+                log_type: log.log_type.clone(),
+                ..Default::default()
+            })
+            .map_err(|e| FilterError::ParseError(e.to_string()))?;
+        Ok(SubgraphLogData {
+            name: event.name.clone(),
+            data: event_data,
+        })
+    }
+}
+#[async_trait::async_trait]
 impl SubgraphFilter for EventFilter {
+    async fn filter_log(
+        &self,
+        block_data: &pb::ethereum::Block,
+    ) -> FilterResult<Vec<SubgraphLogData>> {
+        let eth_block = EthereumBlockData::from(block_data.clone());
+        let logs = block_data
+            .logs
+            .clone()
+            .into_iter()
+            .filter(|log| {
+                &Address::from_str(&log.address).unwrap_or_else(|_| {
+                    panic!(
+                        "parse address log from tx hash {:?} error",
+                        log.transaction_hash
+                    )
+                }) == self.get_address()
+            })
+            .map(web3::types::Log::from)
+            .collect::<Vec<_>>();
+        let mut events = Vec::new();
+        for raw_log in logs.iter() {
+            match self.parse_event(raw_log) {
+                Ok(mut data) => {
+                    data.data.block = eth_block.clone();
+                    let transaction = block_data.transactions.iter().find_map(|tx| {
+                        if H256::from_str(&tx.hash)
+                            .unwrap_or_else(|_| panic!("parse address tx {:?} error", tx))
+                            == raw_log.transaction_hash.unwrap()
+                        {
+                            Some(EthereumTransactionData::from(tx.clone()))
+                        } else {
+                            None
+                        }
+                    });
+                    data.data.transaction = transaction.unwrap();
+                    events.push(data);
+                }
+                Err(e) => {
+                    log::error!("Error parsing event: {:?} from log: {:?}", e, raw_log);
+                }
+            }
+        }
+        Ok(events)
+    }
+
     fn get_contract(&self) -> ethabi::Contract {
         self.contract.clone()
     }
--- src/subgraph_filter/filter.rs
+++ src/subgraph_filter/filter.rs
@@ -1,93 +1,19 @@
 use super::errors::FilterError;
 use super::event_filter::EventFilter;
 use super::event_filter::SubgraphLogData;
-
-use crate::chain::ethereum::block::EthereumBlockData;
-use crate::chain::ethereum::event::EthereumEventData;
-use crate::chain::ethereum::transaction::EthereumTransactionData;
 use crate::ingestor_data as pb;
-use ethabi::Address;
-use std::str::FromStr;
-use web3::types::H160;
-use web3::types::H256;
 
-type FilterResult<T> = Result<T, FilterError>;
+use ethabi::Address;
+use web3::types::H160;
+
+pub type FilterResult<T> = Result<T, FilterError>;
 
 #[async_trait::async_trait]
 pub trait SubgraphFilter {
     async fn filter_log(
         &self,
         block_data: &pb::ethereum::Block,
-    ) -> FilterResult<Vec<SubgraphLogData>> {
-        let eth_block = EthereumBlockData::from(block_data.clone());
-        let logs = block_data
-            .logs
-            .clone()
-            .into_iter()
-            .filter(|log| {
-                &Address::from_str(&log.address).unwrap_or_else(|_| {
-                    panic!(
-                        "parse address log from tx hash {:?} error",
-                        log.transaction_hash
-                    )
-                }) == self.get_address()
-            })
-            .map(web3::types::Log::from)
-            .collect::<Vec<_>>();
-        let mut events = Vec::new();
-        for raw_log in logs.iter() {
-            match self.parse_event(raw_log) {
-                Ok(mut data) => {
-                    data.data.block = eth_block.clone();
-                    let transaction = block_data.transactions.iter().find_map(|tx| {
-                        if H256::from_str(&tx.hash)
-                            .unwrap_or_else(|_| panic!("parse address tx {:?} error", tx))
-                            == raw_log.transaction_hash.unwrap()
-                        {
-                            Some(EthereumTransactionData::from(tx.clone()))
-                        } else {
-                            None
-                        }
-                    });
-                    data.data.transaction = transaction.unwrap();
-                    events.push(data);
-                }
-                Err(e) => {
-                    log::error!("Error parsing event: {:?} from log: {:?}", e, raw_log);
-                }
-            }
-        }
-        Ok(events)
-    }
-
-    fn parse_event(&self, log: &web3::types::Log) -> FilterResult<SubgraphLogData> {
-        let contract = self.get_contract();
-        let event = contract
-            .events()
-            .find(|event| event.signature() == log.topics[0])
-            .ok_or(FilterError::ParseError(format!(
-                "Invalid signature event {}",
-                log.address
-            )))?;
-        let event_data = event
-            .parse_log(ethabi::RawLog {
-                topics: log.topics.clone(),
-                data: log.data.0.clone(),
-            })
-            .map(|event| EthereumEventData {
-                params: event.params,
-                address: log.address,
-                log_index: log.log_index.unwrap_or_default(),
-                transaction_log_index: log.transaction_log_index.unwrap_or_default(),
-                log_type: log.log_type.clone(),
-                ..Default::default()
-            })
-            .map_err(|e| FilterError::ParseError(e.to_string()))?;
-        Ok(SubgraphLogData {
-            name: event.name.clone(),
-            data: event_data,
-        })
-    }
+    ) -> FilterResult<Vec<SubgraphLogData>>;
 
     fn get_contract(&self) -> ethabi::Contract;
 
@@ -98,8 +24,17 @@ pub trait SubgraphFilter {
 pub enum FilterTypes {
     LogEvent(EventFilter),
 }
-
+#[async_trait::async_trait]
 impl SubgraphFilter for FilterTypes {
+    async fn filter_log(
+        &self,
+        block_data: &pb::ethereum::Block,
+    ) -> FilterResult<Vec<SubgraphLogData>> {
+        match self {
+            FilterTypes::LogEvent(filter) => filter.filter_log(block_data).await,
+        }
+    }
+
     fn get_contract(&self) -> ethabi::Contract {
         match self {
             FilterTypes::LogEvent(filter) => filter.get_contract(),
