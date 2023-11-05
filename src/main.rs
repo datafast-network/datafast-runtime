310f79d47958b6ecd84f5524acf255669747a1ba
quannguyen <quanna.dev@gmail.com>
2023-11-05 23:33:26 +0700

    Update remove db

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ async fn main() -> Result<(), SwrError> {
 
     // TODO: impl transform instance
     let transform_instance = match config.transforms.is_some() {
-        true => Some(TransformInstance::new(&config, database.agent().clone())?),
+        true => Some(TransformInstance::new(&config)?),
         false => None,
     };
 
