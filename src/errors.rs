41b78de23e36cdc43c262ce7f12ec7760a199e1e
quannguyen <quanna.dev@gmail.com>
2023-11-08 15:42:30 +0700

    remove unuse error type

--- src/errors.rs
+++ src/errors.rs
@@ -67,12 +67,6 @@ pub enum DatabaseError {
 
 #[derive(Debug, Error)]
 pub enum FilterError {
-    #[error("Filter not found")]
-    NotFound,
-    #[error("Filter invalid address: {0}")]
-    InvalidAddress(String),
-    #[error("Init filter error: {0}")]
-    InitializationError(String),
     #[error(transparent)]
     EthAbiError(#[from] ethabi::Error),
     #[error("Parse error: {0}")]
