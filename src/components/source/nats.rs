5c173e94d947035a58fad780897d3d76f0b3adf0
Vu Tran <me@vutr.io>
2023-11-09 01:35:33 +0700

    refactor nats to use stream! (#53)
    * refactor nats to use stream!
    
    * update
    
    * update
    
    * update

--- src/components/source/nats.rs
+++ src/components/source/nats.rs
@@ -1,11 +1,12 @@
 use crate::config::ContentType;
 use crate::errors::SourceError;
 use crate::messages::SourceDataMessage;
-use kanal::AsyncSender;
+use async_stream::stream;
+use tokio_stream::Stream;
 
 pub struct NatsConsumer {
     conn: nats::Connection,
-    subj: String,
+    subject: String,
     content_type: ContentType,
 }
 
@@ -14,69 +15,75 @@ impl NatsConsumer {
         let conn = nats::connect(uri)?;
         Ok(NatsConsumer {
             conn,
-            subj: subject.to_string(),
+            subject: subject.to_string(),
             content_type,
         })
     }
 
-    fn get_subscription(&self) -> Result<nats::Subscription, SourceError> {
-        self.conn
-            .subscribe(&self.subj)
-            .map_err(SourceError::NatsError)
+    pub fn get_subscription_stream(self) -> impl Stream<Item = SourceDataMessage> {
+        let sub = self
+            .conn
+            .subscribe(&self.subject)
+            .expect("Failed to subscribe to Nats subject");
+
+        stream! {
+            for msg in sub.messages() {
+                let serialized_msg = self.serialize_message(&msg).unwrap();
+                yield serialized_msg;
+                msg.ack().expect("Ack Nats message failed");
+            }
+        }
     }
 
     fn serialize_message(&self, msg: &nats::Message) -> Result<SourceDataMessage, SourceError> {
-        let raw_data = msg.data.clone();
         match self.content_type {
             ContentType::JSON => {
-                let data = serde_json::from_slice(&raw_data)?;
+                let data = serde_json::from_slice(&msg.data)?;
                 Ok(SourceDataMessage::JSON(data))
             }
             ContentType::Protobuf => unimplemented!("Protobuf not implemented yet"),
         }
     }
-
-    pub async fn consume(&self, sender: AsyncSender<SourceDataMessage>) -> Result<(), SourceError> {
-        let sub = self.get_subscription()?;
-        log::info!("Subscribed to subject: {}", self.subj);
-        while let Some(msg) = sub.next() {
-            let source_data = self.serialize_message(&msg)?;
-            log::info!("Received message: {:?}", source_data);
-            sender.send(source_data).await?;
-            msg.ack()?;
-        }
-        Ok(())
-    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::components::source::nats::NatsConsumer;
+    use super::NatsConsumer;
+    use crate::config::ContentType;
     use futures_util::future::join;
+    use futures_util::pin_mut;
     use std::fs::File;
+    use tokio_stream::StreamExt;
 
     #[tokio::test]
-
-    async fn test_send_message() {
+    async fn test_nats() {
         env_logger::try_init().unwrap_or_default();
+
         let block_data = File::open("./src/tests/blocks/block.json").unwrap();
         let block: serde_json::Value = serde_json::from_reader(block_data).unwrap();
         let block_bytes = serde_json::to_vec(&block).unwrap();
         let publisher = nats::connect("localhost").unwrap();
-        let subj = "ethereum";
+        let subject = "ethereum";
+
         let (sender, receive) = kanal::bounded_async::<crate::messages::SourceDataMessage>(1);
-        let sub = NatsConsumer::new(
-            "nats://localhost:4222",
-            subj,
-            crate::config::ContentType::JSON,
-        )
-        .unwrap();
-        log::info!("Sending message");
+
+        let sub = NatsConsumer::new("nats://localhost:4222", subject, ContentType::JSON).unwrap();
+
+        log::info!("Setup tasks");
+
         let t1 = async {
-            publisher.publish(subj, &block_bytes).unwrap();
+            publisher.publish(subject, &block_bytes).unwrap();
         };
-        let t3 = sub.consume(sender);
-        let t2 = async {
+
+        let t2 = async move {
+            let s = sub.get_subscription_stream();
+            pin_mut!(s);
+            while let Some(msg) = s.next().await {
+                sender.send(msg).await.unwrap();
+            }
+        };
+
+        let t3 = async {
             while let Ok(msg) = receive.recv().await {
                 log::info!("Received message: {:?}", msg);
                 return;
@@ -84,10 +91,10 @@ mod tests {
         };
 
         tokio::select! {
-            _ = t2 => {
+            _ = t3 => {
                 log::info!("receive done");
             },
-            _ = join(t1, t3) => {
+            _ = join(t1, t2) => {
                 log::info!("t1 and t3 done");
             }
         }
