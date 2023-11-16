c84790a1896943f731a2d94b24c09923fa3a3093
vutran1710 <me@vutr.io>
2023-11-16 12:53:40 +0700

    up

--- src/components/manifest_loader/local.rs
+++ src/components/manifest_loader/local.rs
@@ -10,7 +10,6 @@ pub struct LocalFileLoader {
     pub subgraph_dir: String,
     pub subgraph_yaml: SubgraphYaml,
     pub abis: HashMap<String, serde_json::Value>,
-    pub schemas:
 }
 
 #[async_trait]
@@ -110,6 +109,11 @@ impl LoaderTrait for LocalFileLoader {
 
 #[cfg(test)]
 mod test {
+    use apollo_parser::cst::Definition;
+    use apollo_parser::Parser;
+    use log::info;
+    use std::fs::read_to_string;
+
     use super::*;
 
     #[tokio::test]
@@ -125,5 +129,37 @@ mod test {
         loader.load_wasm("TestTypes").await.unwrap();
         loader.load_wasm("TestStore").await.unwrap();
         loader.load_wasm("TestDataSource").await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn test_parse_graphql_schema() {
+        env_logger::try_init().unwrap_or_default();
+        let loader = LocalFileLoader::new("../subgraph-testing/packages/v0_0_5")
+            .await
+            .unwrap();
+
+        let gql =
+            read_to_string("../subgraph-testing/packages/v0_0_5/build/schema.graphql").unwrap();
+
+        let parser = Parser::new(&gql);
+        let ast = parser.parse();
+        let doc = ast.document();
+        for def in doc.definitions() {
+            if let Definition::ObjectTypeDefinition(object) = def {
+                info!("Entity: {:?}", object.name().unwrap().text());
+
+                for field in object.fields_definition().unwrap().field_definitions() {
+                    // As this loops, it will print the two field
+                    // names it encounters:
+                    // field name: name
+                    // field name: favSnack
+                    info!(
+                        "field description= {:?}",
+                        // field.name().unwrap().text(),
+                        field.arguments_definition()
+                    );
+                }
+            }
+        }
     }
 }
