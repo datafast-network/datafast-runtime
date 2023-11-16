500a7c8450241808b68090be04a08e28567cf676
quannguyen <quanna.dev@gmail.com>
2023-11-16 20:13:34 +0700

    update

--- src/runtime/wasm_host/chain/ethereum.rs
+++ src/runtime/wasm_host/chain/ethereum.rs
@@ -1,3 +1,71 @@
+use crate::chain::ethereum::asc::EthereumValueKind;
+use crate::errors::AscError;
+use crate::runtime::asc::base::asc_get;
+use crate::runtime::asc::base::asc_new;
+use crate::runtime::asc::base::AscPtr;
+use crate::runtime::asc::native_types::r#enum::AscEnum;
+use crate::runtime::asc::native_types::r#enum::AscEnumArray;
+use crate::runtime::asc::native_types::string::AscString;
+use crate::runtime::asc::native_types::Uint8Array;
+use crate::runtime::wasm_host::Env;
+use ethabi::decode;
+use ethabi::param_type::Reader;
+use tiny_keccak::Hasher;
+use wasmer::FunctionEnvMut;
+
+pub fn ethereum_encode(
+    mut fenv: FunctionEnvMut<Env>,
+    token: AscPtr<AscEnum<EthereumValueKind>>,
+) -> Result<AscPtr<Uint8Array>, AscError> {
+    let token_ptr: ethabi::Token = asc_get(&fenv, token, 0)?;
+
+    let bytes = ethabi::encode(&[token_ptr]);
+    let asc_bytes = asc_new(&mut fenv, bytes.as_slice())?;
+    Ok(asc_bytes)
+}
+
+pub fn ethereum_decode(
+    mut fenv: FunctionEnvMut<Env>,
+    types_ptr: AscPtr<AscString>,
+    data_ptr: AscPtr<Uint8Array>,
+) -> Result<AscPtr<AscEnum<EthereumValueKind>>, AscError> {
+    let types: String = asc_get(&fenv, types_ptr, 0)?;
+    let data: Vec<u8> = asc_get(&fenv, data_ptr, 0)?;
+    let param_types = Reader::read(&types)
+        .map_err(|_| AscError::Plain("ethereum decode types error".to_string()))?;
+
+    let data = decode(&[param_types], &data)
+        // The `.pop().unwrap()` here is ok because we're always only passing one
+        // `param_types` to `decode`, so the returned `Vec` has always size of one.
+        // We can't do `tokens[0]` because the value can't be moved out of the `Vec`.
+        .map(|mut tokens| tokens.pop().unwrap())
+        .map_err(|_| AscError::Plain("ethereum decode token error".to_string()))?;
+    let asc_data = asc_new(&mut fenv, &data)?;
+    Ok(asc_data)
+}
+
+pub fn crypto_keccak_256(
+    mut fenv: FunctionEnvMut<Env>,
+    input_ptr: AscPtr<Uint8Array>,
+) -> Result<AscPtr<Uint8Array>, AscError> {
+    let input: Vec<u8> = asc_get(&fenv, input_ptr, 0)?;
+    let data = &input[..];
+    let mut hash = tiny_keccak::Keccak::v256();
+    let mut output = [0u8; 32];
+    hash.update(data);
+    hash.finalize(&mut output);
+    let hash_256 = web3::types::H256::from_slice(&output);
+    let asc_data = asc_new(&mut fenv, &hash_256)?;
+    Ok(asc_data)
+}
+
+pub fn ethereum_call(
+    _fenv: FunctionEnvMut<Env>,
+    _wasm_ptr: i32,
+) -> Result<AscEnumArray<EthereumValueKind>, AscError> {
+    todo!()
+}
+
 #[cfg(test)]
 mod test {
     use crate::chain::ethereum::block::EthereumBlockData;
