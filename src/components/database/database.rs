0426337ddc4e85a9e3eee905fe8ce47ba311582b
vutran1710 <me@vutr.io>
2023-11-16 12:08:06 +0700

    Impl more methods for Database2

--- src/components/database/database.rs
+++ src/components/database/database.rs
@@ -1,13 +1,15 @@
 use super::extern_db::ExternDB;
+use super::extern_db::ExternDBTrait;
 use super::memory_db::MemoryDb;
 use super::schema_lookup::SchemaLookup;
+use super::RawEntity;
 use crate::config::Config;
 use crate::errors::DatabaseError;
+use crate::messages::EntityID;
 use crate::messages::EntityType;
 use crate::messages::StoreOperationMessage;
 use crate::messages::StoreRequestResult;
 use crate::runtime::asc::native_types::store::Value;
-use std::collections::HashMap;
 
 pub struct Database2 {
     pub mem: MemoryDb,
@@ -21,29 +23,74 @@ impl Database2 {
         Ok(Database2 { mem, db })
     }
 
-    pub async fn handle_store_request(
+    async fn handle_store_request(
         &mut self,
         message: StoreOperationMessage,
     ) -> Result<StoreRequestResult, DatabaseError> {
         match message {
             StoreOperationMessage::Create(data) => self.handle_create(data).await,
-            StoreOperationMessage::Load(data) => unimplemented!(),
-            StoreOperationMessage::Update(_) => unimplemented!(),
-            StoreOperationMessage::Delete(_) => unimplemented!(),
+            StoreOperationMessage::Load(data) => self.handle_load(data).await,
+            StoreOperationMessage::Update(data) => self.handle_update(data).await,
+            StoreOperationMessage::Delete(data) => self.handle_delete(data).await,
         }
     }
 
     async fn handle_create(
         &mut self,
-        data: (EntityType, HashMap<String, Value>),
+        data: (EntityType, RawEntity),
     ) -> Result<StoreRequestResult, DatabaseError> {
         let (entity_type, data) = data;
-        let entity_id = data.get("id").cloned().unwrap();
+        let entity_id = data.get("id").cloned().expect("Missing ID in RawEntity");
         self.mem.create_entity(entity_type, data)?;
+
         if let Value::String(entity_id) = entity_id {
             Ok(StoreRequestResult::Create(entity_id))
         } else {
-            Err(DatabaseError::InvalidValue("id".to_string()))
+            Err(DatabaseError::InvalidValue("id is not string".to_string()))
         }
+    }
+
+    async fn handle_load(
+        &mut self,
+        data: (EntityType, EntityID),
+    ) -> Result<StoreRequestResult, DatabaseError> {
+        let (entity_type, entity_id) = data;
+
+        let entity = self
+            .mem
+            .load_entity_latest(entity_type.clone(), entity_id.clone())?;
+
+        if entity.is_none() {
+            let entity = self.db.load_entity_latest(&entity_type, &entity_id).await?;
+
+            if entity.is_none() {
+                return Ok(StoreRequestResult::Load(None));
+            }
+
+            let data = entity.unwrap();
+            self.mem.create_entity(entity_type, data.clone())?;
+            return Ok(StoreRequestResult::Load(Some(data)));
+        }
+
+        let data = entity.unwrap();
+        return Ok(StoreRequestResult::Load(Some(data)));
+    }
+
+    async fn handle_update(
+        &mut self,
+        data: (EntityType, EntityID, RawEntity),
+    ) -> Result<StoreRequestResult, DatabaseError> {
+        let (entity_type, _entity_id, data) = data;
+        self.handle_create((entity_type, data)).await?;
+        Ok(StoreRequestResult::Update)
+    }
+
+    async fn handle_delete(
+        &mut self,
+        data: (EntityType, EntityID),
+    ) -> Result<StoreRequestResult, DatabaseError> {
+        let (entity_type, entity_id) = data;
+        self.mem.soft_delete(entity_type, entity_id)?;
+        Ok(StoreRequestResult::Delete)
     }
 }
