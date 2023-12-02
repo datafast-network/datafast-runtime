ef43f273cb7f324466741cea7b3f1ce318a64a47
vutran1710 <me@vutr.io>
2023-12-03 03:59:30 +0700

    update test & logs

--- src/components/progress_ctrl/mod.rs
+++ src/components/progress_ctrl/mod.rs
@@ -1,15 +1,19 @@
 use crate::common::BlockPtr;
 use crate::common::Source;
+use crate::critical;
+use crate::error;
+use crate::info;
+use crate::warn;
 use std::collections::VecDeque;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum ProgressCheckResult {
     OkToProceed,
     BlockAlreadyProcessed,
-    UnexpectedBlock { expected: u64, received: u64 },
+    UnexpectedBlock,
     MaybeReorg,
     ForkBlock,
-    UnrecognizedBlock(BlockPtr),
+    UnrecognizedBlock,
 }
 
 #[derive(Clone)]
@@ -53,10 +57,13 @@ impl ProgressCtrl {
                     return ProgressCheckResult::OkToProceed;
                 }
 
-                return ProgressCheckResult::UnexpectedBlock {
-                    expected: min_start_block,
-                    received: new_block_ptr.number,
-                };
+                error!(
+                    ProgressCtrl,
+                    "received an unexpected block whose number does not match subgraph's required start-block";
+                    expected_block_number => min_start_block,
+                    received_block_number => new_block_ptr.number
+                );
+                return ProgressCheckResult::UnexpectedBlock;
             }
             Some(last_processed) => {
                 if last_processed.is_parent(&new_block_ptr) {
@@ -68,22 +75,58 @@ impl ProgressCtrl {
                 }
 
                 if new_block_ptr.number > last_processed.number + 1 {
-                    return ProgressCheckResult::UnexpectedBlock {
-                        expected: last_processed.number + 1,
-                        received: new_block_ptr.number,
-                    };
+                    critical!(
+                        ProgressCtrl,
+                        "received an invalid block whose number is larger than expected";
+                        expected_block_number => last_processed.number + 1,
+                        received_block_number => new_block_ptr.number
+                    );
+                    return ProgressCheckResult::UnexpectedBlock;
                 }
 
                 if new_block_ptr.number < self.recent_block_ptrs.back().unwrap().number {
-                    return ProgressCheckResult::UnrecognizedBlock(new_block_ptr.clone());
+                    critical!(
+                        ProgressCtrl,
+                        r#"
+Block not recognized!
+Please check your setup - as it can be either:
+1) a reorg is too deep for runtime to handle, or
+2) you have set a reorg-threshold which is too shallow, or
+3) you are using a WRONG block source, or
+4) Data-store & subgraph's block-pointers do not match!
+"#;
+                        received_block => new_block_ptr,
+                        recent_blocks_processed => format!(
+                            "{} ... {}",
+                            self.recent_block_ptrs.back().unwrap(),
+                            last_processed
+                        )
+                    );
+                    return ProgressCheckResult::UnrecognizedBlock;
                 }
 
                 for block in self.recent_block_ptrs.iter() {
                     if *block == new_block_ptr {
+                        if new_block_ptr.number % 10 == 0 {
+                            warn!(
+                                ProgressCtrl,
+                                "Received a block that was already processed before";
+                                block => new_block_ptr
+                            );
+                        }
                         return ProgressCheckResult::BlockAlreadyProcessed;
                     }
 
                     if block.is_parent(&new_block_ptr) {
+                        info!(
+                            ProgressCtrl,
+                            "Reorg happened and a proper fork-block received";
+                            fork_block => new_block_ptr,
+                            parent_block => block
+                        );
+                        self.recent_block_ptrs
+                            .retain(|b| b.number < new_block_ptr.number);
+                        self.recent_block_ptrs.push_front(new_block_ptr);
                         return ProgressCheckResult::ForkBlock;
                     }
                 }
@@ -107,8 +150,7 @@ mod tests {
             abi: "".to_owned(),
             startBlock: start_block,
         }];
-        let reorg_threshold = 10;
-        let mut pc = ProgressCtrl::new(vec![], sources, reorg_threshold);
+        let mut pc = ProgressCtrl::new(vec![], sources, 10);
         assert!(pc.recent_block_ptrs.is_empty());
 
         let actual_start_block = pc.get_min_start_block();
@@ -128,7 +170,7 @@ mod tests {
                     });
                     assert_eq!(result, ProgressCheckResult::OkToProceed);
                 }
-                assert_eq!(pc.recent_block_ptrs.len(), reorg_threshold as usize);
+                assert_eq!(pc.recent_block_ptrs.len(), 10);
                 assert_eq!(pc.recent_block_ptrs.front().unwrap().number, 19);
                 assert_eq!(pc.recent_block_ptrs.back().unwrap().number, 10);
 
@@ -138,10 +180,7 @@ mod tests {
                         hash: "".to_string(),
                         parent_hash: "".to_string()
                     }),
-                    ProgressCheckResult::UnexpectedBlock {
-                        expected: 20,
-                        received: 22
-                    }
+                    ProgressCheckResult::UnexpectedBlock
                 );
 
                 assert_eq!(
@@ -150,10 +189,7 @@ mod tests {
                         hash: "".to_string(),
                         parent_hash: "".to_string()
                     }),
-                    ProgressCheckResult::UnexpectedBlock {
-                        expected: 20,
-                        received: 21
-                    }
+                    ProgressCheckResult::UnexpectedBlock
                 );
 
                 assert_eq!(
@@ -189,11 +225,7 @@ mod tests {
                         hash: "".to_string(),
                         parent_hash: "".to_string(),
                     }),
-                    ProgressCheckResult::UnrecognizedBlock(BlockPtr {
-                        number: 9,
-                        hash: "".to_string(),
-                        parent_hash: "".to_string(),
-                    }),
+                    ProgressCheckResult::UnrecognizedBlock,
                 );
 
                 assert_eq!(
@@ -249,6 +281,26 @@ mod tests {
                         parent_hash: "n=10".to_string(),
                     }
                 );
+
+                assert_eq!(
+                    pc.check_block(BlockPtr {
+                        number: 19,
+                        hash: "n=fork19".to_string(),
+                        parent_hash: "n=18".to_string(),
+                    }),
+                    ProgressCheckResult::ForkBlock
+                );
+
+                assert_eq!(pc.recent_block_ptrs.len(), 9);
+                assert_eq!(
+                    pc.recent_block_ptrs.front().cloned().unwrap(),
+                    BlockPtr {
+                        number: 19,
+                        hash: "n=fork19".to_string(),
+                        parent_hash: "n=18".to_string(),
+                    }
+                );
+                assert_eq!(pc.recent_block_ptrs.back().unwrap().number, 11);
             }
             Some(block_number) => assert_eq!(actual_start_block, block_number),
         }
