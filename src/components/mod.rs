9ca70850e54cd5d2d3d2ee6bf661caeafcee50ba
vutran1710 <me@vutr.io>
2023-12-03 04:39:25 +0700

    refactor

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,12 +1,12 @@
-mod customs;
+mod inspector;
 mod manifest_loader;
 mod source;
 mod subgraph;
 mod subgraph_filter;
 mod valve;
 
-pub use customs::BlockInspectionResult;
-pub use customs::Inspector;
+pub use inspector::BlockInspectionResult;
+pub use inspector::Inspector;
 pub use manifest_loader::ManifestLoader;
 pub use source::BlockSource;
 pub use subgraph::Subgraph;
