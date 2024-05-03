b4ceb1ba48cf9e41a918862d5534cd78fee998a1
quannguyen <quanna.dev@gmail.com>
2024-05-03 20:20:07 +0700

    implement asc pointer for transaction receipt

--- src/chain/ethereum/transaction.rs
+++ src/chain/ethereum/transaction.rs
@@ -1,4 +1,5 @@
 use super::asc::*;
+use crate::chain::ethereum::log::AscLogArray;
 use crate::errors::AscError;
 use crate::impl_asc_type_struct;
 use crate::runtime::asc::base::asc_get;
@@ -15,11 +16,14 @@ use crate::runtime::asc::native_types::Uint8Array;
 use crate::runtime::bignumber::bigint::BigInt;
 use ethabi::Bytes;
 use semver::Version;
+use web3::types::Log;
 use web3::types::Transaction;
+use web3::types::TransactionReceipt;
 use web3::types::H160;
 use web3::types::H256;
 use web3::types::U128;
 use web3::types::U256;
+use web3::types::U64;
 
 #[repr(C)]
 pub struct AscEthereumTransaction {
@@ -142,4 +146,153 @@ impl ToAscObj<AscTransactionArray> for Vec<EthereumTransactionData> {
 
 impl AscIndexId for AscTransactionArray {
     const INDEX_ASC_TYPE_ID: IndexForAscTypeId = IndexForAscTypeId::ArrayEthereumTransaction;
+}
+
+#[repr(C)]
+pub struct AscTransactionReceipt {
+    pub transaction_hash: AscPtr<Uint8Array>,
+    pub transaction_index: AscPtr<AscBigInt>,
+    pub block_hash: AscPtr<Uint8Array>,
+    pub block_number: AscPtr<AscBigInt>,
+    pub cumulative_gas_used: AscPtr<AscBigInt>,
+    pub gas_used: AscPtr<AscBigInt>,
+    pub contract_address: AscPtr<AscH160>,
+    pub logs: AscPtr<AscLogArray>,
+    pub status: AscPtr<AscBigInt>,
+    pub root: AscPtr<Uint8Array>,
+    pub logs_bloom: AscPtr<Uint8Array>,
+}
+
+impl AscIndexId for AscTransactionReceipt {
+    const INDEX_ASC_TYPE_ID: IndexForAscTypeId = IndexForAscTypeId::TransactionReceipt;
+}
+
+impl_asc_type_struct!(
+    AscTransactionReceipt;
+    transaction_hash => AscPtr<Uint8Array>,
+    transaction_index => AscPtr<AscBigInt>,
+    block_hash => AscPtr<Uint8Array>,
+    block_number => AscPtr<AscBigInt>,
+    cumulative_gas_used => AscPtr<AscBigInt>,
+    gas_used => AscPtr<AscBigInt>,
+    contract_address => AscPtr<AscH160>,
+    logs => AscPtr<AscLogArray>,
+    status => AscPtr<AscBigInt>,
+    root => AscPtr<Uint8Array>,
+    logs_bloom => AscPtr<Uint8Array>
+);
+
+#[derive(Clone, Debug, Default)]
+pub struct EthereumTransactionReceipt {
+    pub transaction_hash: H256,
+    pub transaction_index: U64,
+    pub block_hash: H256,
+    pub block_number: U256,
+    pub cumulative_gas_used: U256,
+    pub gas_used: Option<U256>,
+    pub contract_address: Option<H160>,
+    pub logs: Vec<Log>,
+    pub status: Option<U64>,
+    pub root: Option<H256>,
+    pub logs_bloom: Bytes,
+}
+
+impl From<TransactionReceipt> for EthereumTransactionReceipt {
+    fn from(receipt: TransactionReceipt) -> EthereumTransactionReceipt {
+        EthereumTransactionReceipt {
+            transaction_hash: receipt.transaction_hash,
+            transaction_index: receipt.transaction_index,
+            block_hash: receipt.block_hash.unwrap(),
+            block_number: receipt.block_number.unwrap().as_u64().into(),
+            cumulative_gas_used: receipt.cumulative_gas_used,
+            gas_used: receipt.gas_used,
+            contract_address: receipt.contract_address,
+            logs: receipt.logs,
+            status: receipt.status,
+            root: receipt.root,
+            logs_bloom: receipt.logs_bloom.as_bytes().to_vec(),
+        }
+    }
+}
+
+impl FromAscObj<AscTransactionReceipt> for EthereumTransactionReceipt {
+    fn from_asc_obj<H: AscHeap + ?Sized>(
+        obj: AscTransactionReceipt,
+        heap: &H,
+        _depth: usize,
+    ) -> Result<Self, AscError> {
+        Ok(EthereumTransactionReceipt {
+            transaction_hash: asc_get(heap, obj.transaction_hash, 0)?,
+            transaction_index: asc_get(heap, obj.transaction_index, 0)?,
+            block_hash: asc_get(heap, obj.block_hash, 0)?,
+            block_number: asc_get(heap, obj.block_number, 0)?,
+            cumulative_gas_used: asc_get(heap, obj.cumulative_gas_used, 0)?,
+            gas_used: asc_get_optional(heap, obj.gas_used, 0)?,
+            contract_address: asc_get_optional(heap, obj.contract_address, 0)?,
+            logs: asc_get(heap, obj.logs, 0)?,
+            status: asc_get_optional(heap, obj.status, 0)?,
+            root: asc_get_optional(heap, obj.root, 0)?,
+            logs_bloom: asc_get(heap, obj.logs_bloom, 0)?,
+        })
+    }
+}
+
+impl ToAscObj<AscTransactionReceipt> for EthereumTransactionReceipt {
+    fn to_asc_obj<H: AscHeap + ?Sized>(
+        &self,
+        heap: &mut H,
+    ) -> Result<AscTransactionReceipt, AscError> {
+        Ok(AscTransactionReceipt {
+            transaction_hash: asc_new(heap, &self.transaction_hash)?,
+            transaction_index: asc_new(heap, &BigInt::from(self.transaction_index))?,
+            block_hash: asc_new(heap, &self.block_hash)?,
+            block_number: asc_new(heap, &BigInt::from_unsigned_u256(&self.block_number))?,
+            cumulative_gas_used: asc_new(
+                heap,
+                &BigInt::from_unsigned_u256(&self.cumulative_gas_used),
+            )?,
+            gas_used: self
+                .gas_used
+                .map(|gas_used| asc_new(heap, &BigInt::from_unsigned_u256(&gas_used)))
+                .unwrap_or(Ok(AscPtr::null()))?,
+            contract_address: self
+                .contract_address
+                .map(|contract_address| asc_new(heap, &contract_address))
+                .unwrap_or(Ok(AscPtr::null()))?,
+            logs: asc_new(heap, &self.logs)?,
+            status: self
+                .status
+                .map(|status| asc_new(heap, &BigInt::from(status)))
+                .unwrap_or(Ok(AscPtr::null()))?,
+            root: self
+                .root
+                .map(|root| asc_new(heap, &root))
+                .unwrap_or(Ok(AscPtr::null()))?,
+            logs_bloom: asc_new(heap, &*self.logs_bloom)?,
+        })
+    }
+}
+
+impl From<(Transaction, Vec<Log>)> for EthereumTransactionReceipt {
+    fn from((tx, logs): (Transaction, Vec<Log>)) -> EthereumTransactionReceipt {
+        let mut logs_of_tx = logs
+            .into_iter()
+            .filter(|log| log.transaction_hash.unwrap() == tx.hash)
+            .collect::<Vec<Log>>();
+        logs_of_tx.sort_by_key(|log| log.log_index.unwrap().as_u64());
+
+        EthereumTransactionReceipt {
+            transaction_hash: tx.hash,
+            transaction_index: tx.transaction_index.unwrap().as_u64().into(),
+            block_hash: tx.block_hash.unwrap(),
+            block_number: tx.block_number.unwrap().as_u64().into(),
+            cumulative_gas_used: U256::zero(), //todo: get cumulative gas used
+            gas_used: None,                    //todo: get gas used
+            contract_address: None,            //todo: get contract address
+            logs: logs_of_tx,
+            status: None,
+            root: None,
+            logs_bloom: Bytes::default(),
+        }
+    }
 }
