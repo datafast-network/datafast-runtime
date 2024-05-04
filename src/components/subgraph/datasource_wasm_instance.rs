31cadb84522f15db3a33ba072e21bcd78421c429
quannguyen <quanna.dev@gmail.com>
2024-05-04 13:29:17 +0700

    add test invoke transaction receipt

--- src/components/subgraph/datasource_wasm_instance.rs
+++ src/components/subgraph/datasource_wasm_instance.rs
@@ -134,3 +134,94 @@ impl DatasourceWasmInstance {
         (self.host.current_ptr() as f32) > Self::MAXIMUM_HEAP_SIZE
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::chain::ethereum::transaction::EthereumTransactionReceipt;
+    use crate::rpc_client::RpcAgent;
+    use crate::runtime::wasm_host::test::mock_wasm_host;
+    use num_bigint::BigInt;
+    use prometheus::Registry;
+    use semver::Version;
+    use std::str::FromStr;
+    use web3::types::Address;
+    use web3::types::Log;
+    use web3::types::H160;
+    use web3::types::H256;
+    use web3::types::U256;
+    use web3::types::U64;
+
+    #[test]
+    fn test_transaction_receipt_invoke() {
+        env_logger::try_init().unwrap_or_default();
+        let registry = Registry::default();
+        let host = mock_wasm_host(
+            Version::parse("0.0.5").unwrap(),
+            "../subgraph-testing/packages/v0_0_5/build/TestTransaction/TestTransaction.wasm",
+            &registry,
+            RpcAgent::new_mock(&registry),
+        );
+        let tx_handler = Handler::new(&host.instance.exports, "testTransaction").unwrap();
+        let mut txs_handlers = HashMap::new();
+        txs_handlers.insert("testTransaction".to_string(), tx_handler);
+        let mut ds = DatasourceWasmInstance {
+            name: "TestTransaction".to_string(),
+            host,
+            ethereum_handlers: EthereumHandlers {
+                block: HashMap::new(),
+                transaction: txs_handlers,
+                events: HashMap::new(),
+            },
+        };
+        let mut logs = vec![];
+        logs.push(Log {
+            address: H160::from_str("0xdAC17F958D2ee523a2206206994597C13D831ec7").unwrap(),
+            topics: vec![
+                H256::from_str(
+                    "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+                )
+                .unwrap(),
+                H256::from(
+                    Address::from_str("0x9FA7fC17A8d0151F6641a636cC734064E0C64093").unwrap(),
+                ),
+                H256::from(
+                    Address::from_str("0xFEb6A9de89465dA662Ff16F85b5342B73bD0B455").unwrap(),
+                ),
+            ],
+            data: BigInt::from_str("27530167")
+                .unwrap()
+                .to_signed_bytes_le()
+                .into(),
+            block_hash: None,
+            block_number: None,
+            transaction_hash: None,
+            transaction_index: None,
+            log_index: None,
+            transaction_log_index: None,
+            log_type: None,
+            removed: None,
+        });
+        let tx_receipt = EthereumTransactionReceipt {
+            block_number: U64::from(19722402),
+            block_hash: H256::from_str(
+                "0x2b4e13e0bd4996253f6f7ef66ca2f63ee21de8f29c572dba23f5efbb762dade7",
+            )
+            .unwrap(),
+            transaction_hash: H256::from_str(
+                "0xd06f33b2193b1ecd0ed6d8bfb81a4ee31a4c4754dd6f1d42980a0f7da35a14bb",
+            )
+            .unwrap(),
+            transaction_index: U64::from(0),
+            cumulative_gas_used: U256::zero(),
+            logs,
+            ..Default::default()
+        };
+        ds.invoke(
+            HandlerTypes::EthereumTransaction,
+            "testTransaction",
+            tx_receipt,
+        )
+        .unwrap();
+    }
+}
